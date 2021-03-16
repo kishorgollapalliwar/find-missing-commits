c0be40b18c03e089a1e935186f733d6d8dc3e150
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2021-03-16 07:25:27 +0530

    Added documentation to GitCommitTimeComparator

--- src/main/java/com/kishor/git/commit/comparator/GitCommitTimeComparator.java
+++ src/main/java/com/kishor/git/commit/comparator/GitCommitTimeComparator.java
@@ -4,11 +4,18 @@ import java.util.Comparator;
 
 import com.kishor.git.model.Commit;
 
+/**
+ * A GIT commit comparator based on commit time.
+ * @author kishor
+ *
+ */
 public class GitCommitTimeComparator implements Comparator<Commit> {
 
+    /**
+     * Compares two GIT commits by their commit time.
+     */
     @Override
     public int compare(Commit commit1, Commit commit2) {
         return commit1.getTime().compareTo(commit2.getTime());
     }
-
 }
