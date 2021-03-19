49230fff4d07f1e4678dad745b8f9d18ab15265a
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2021-03-19 06:26:53 +0530

    CommitReader: Parent interface to family of commit reader classes

---
+++ src/main/java/com/kishor/git/commit/CommitReader.java
@@ -0,0 +1,17 @@
+package com.kishor.git.commit;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import com.kishor.git.model.Commit;
+
+/**
+ * Interface representing family of classes which will be used to read commit file
+ * @author kishor
+ *
+ */
+public interface CommitReader {
+    public void init(Properties props);
+    public Map<String, List<Commit>> read();
+}
