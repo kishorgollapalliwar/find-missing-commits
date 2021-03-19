d9d68aab15df1a16480efeedea8cb8f81c775f79
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2021-03-19 06:19:43 +0530

    CommitFileReader: Added class to read commits from file

---
+++ src/main/java/com/kishor/git/commit/CommitFileReader.java
@@ -0,0 +1,10 @@
+package com.kishor.git.commit;
+
+/**
+ * Responsibility of this class is to read commit file and generate {@link java.util.Map map} of commits
+ * @author kishor
+ *
+ */
+public class CommitFileReader {
+
+}
