82c03416771f56fdfa53faca9999b423894cb752
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2021-03-20 06:32:15 +0530

    CommitFileReader: Made it part of CommitReader family

--- src/main/java/com/kishor/git/commit/CommitFileReader.java
+++ src/main/java/com/kishor/git/commit/CommitFileReader.java
@@ -1,10 +1,28 @@
 package com.kishor.git.commit;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import com.kishor.git.model.Commit;
+
 /**
  * Responsibility of this class is to read commit file and generate {@link java.util.Map map} of commits
  * @author kishor
  *
  */
-public class CommitFileReader {
+public class CommitFileReader implements CommitReader{
+
+    @Override
+    public void init(Properties props) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Map<String, List<Commit>> read() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
