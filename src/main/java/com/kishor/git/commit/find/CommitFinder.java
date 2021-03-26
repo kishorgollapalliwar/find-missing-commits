f30cad162bfd2762f73b78c7ea3ba43780c9129b
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2021-03-26 09:12:23 +0530

    CommitFinder: Added init & find method

--- src/main/java/com/kishor/git/commit/find/CommitFinder.java
+++ src/main/java/com/kishor/git/commit/find/CommitFinder.java
@@ -1,5 +1,12 @@
 package com.kishor.git.commit.find;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeSet;
+
+import com.kishor.git.model.Commit;
+
 /**
  * Responsibility of this class is to find missing commits.
  * @author kishor
@@ -7,4 +14,11 @@ package com.kishor.git.commit.find;
  */
 public class CommitFinder {
 
+    public void init(Properties props) {
+        
+    }
+
+    public TreeSet<Commit> find(Map<String, List<Commit>> base, Map<String, List<Commit>> other) {
+        return null;
+    }
 }
