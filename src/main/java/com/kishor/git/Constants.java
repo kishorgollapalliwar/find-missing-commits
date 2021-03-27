fafac6b0b51102a8f90d0ee354c85be51ed4cc7f
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2021-03-27 21:22:29 +0530

    Constants: Added constants to file

--- src/main/java/com/kishor/git/Constants.java
+++ src/main/java/com/kishor/git/Constants.java
@@ -7,5 +7,7 @@ package com.kishor.git;
  */
 
 public class Constants {
-
+    public static final String INPUT_FILE_PATH_PROPS_KEY = "input.file.path";
+    public static final String IGNORE_KEYWARDS_PROPS_KEY = "ignore.keywords";
+    public static final String IGNORE_KEYWARDS_DEFAULT   = "Jenkins,Merge";
 }
