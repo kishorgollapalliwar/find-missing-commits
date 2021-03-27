9a2e32a3842bb1d2540e623f37ac0b9eeabbb337
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2021-03-27 21:29:16 +0530

    CommitFinder: implemented init method

--- src/main/java/com/kishor/git/commit/find/CommitFinder.java
+++ src/main/java/com/kishor/git/commit/find/CommitFinder.java
@@ -1,10 +1,13 @@
 package com.kishor.git.commit.find;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import java.util.TreeSet;
 
+import com.kishor.git.Constants;
 import com.kishor.git.model.Commit;
 
 /**
@@ -13,9 +16,13 @@ import com.kishor.git.model.Commit;
  *
  */
 public class CommitFinder {
+    private Set<String> ignoreKeywordSet = new HashSet<>();
 
     public void init(Properties props) {
-        
+        String [] ignoreKeywords = props.getProperty(Constants.IGNORE_KEYWARDS_PROPS_KEY).split(",");
+        for (int index=0; index < ignoreKeywords.length; index++) {
+            ignoreKeywordSet.add(ignoreKeywords[index]);
+        }
     }
 
     public TreeSet<Commit> find(Map<String, List<Commit>> base, Map<String, List<Commit>> other) {
