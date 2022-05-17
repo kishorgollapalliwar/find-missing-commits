65b2fe6059b04d95f9e560c5a7db27f85f2c8035
Kishor Gollapalliwar <kishor.gollapalliwar@gmail.com>
2022-05-17 15:10:17 +0530

    Added feature: Debug file generation

--- src/main/java/com/kishor/git/App.java
+++ src/main/java/com/kishor/git/App.java
@@ -3,8 +3,10 @@ package com.kishor.git;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
 
@@ -14,41 +16,103 @@ import com.kishor.git.commit.read.CommitReader;
 import com.kishor.git.model.Commit;
 
 /**
- * Main class
- * Responsibility of this class is as follows
+ * Main class Responsibility of this class is as follows
  * <ol>
- *  <li>Input validation</li>
- *  <li>Parse input</li>
- *  <li>Delegate commit finding functionality</li>
- * </ol>to start the commit finding process
+ * <li>Input validation</li>
+ * <li>Parse input</li>
+ * <li>Delegate commit finding functionality</li>
+ * </ol>
+ * to start the commit finding process
  *
  * @author kishor
  */
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         System.out.println("==================== Start ==================== ");
-        //#1. Input Validation
+        // #1. Input Validation
         validateArguments(args);
 
-        //#2. Input parsing/ cleansing
-        String inputDirectory = args[0];
-        String srcBranchName  = args[1]; // source branch from where commit will be pulled
-        String destBranchName = args[2]; // destination branch to which branch commit will be pushed
+        // #2. Input parsing/ cleansing
+        String  inputDirectory   = args[0];
+        String  srcBranchName    = args[1]; // source branch from where commit will be pulled
+        String  destBranchName   = args[2]; // destination branch to which branch commit will be pushed
+        boolean enableDebugFiles = false;
+
+        if (args.length == 4 && "EnableDebug".equalsIgnoreCase(args[3])) {
+            enableDebugFiles = true;
+        }
 
         if (!inputDirectory.endsWith(File.separator)) {
             inputDirectory = inputDirectory + File.separator;
         }
 
-        //#3 Read base branch commits
-        CommitReader srcCommitReader = getCommitFileReader(inputDirectory+File.separator+srcBranchName);
-        Map<String, List<Commit>> srcBranch = srcCommitReader.read();
+        // #3 Read base branch commits
+        CommitReader              srcCommitReader = getCommitFileReader(inputDirectory + File.separator + srcBranchName);
+        Map<String, List<Commit>> srcBranch       = srcCommitReader.read();
 
-        //#4 Read other branch commits
-        CommitReader destCommitReader = getCommitFileReader(inputDirectory+File.separator+destBranchName);
-        Map<String, List<Commit>> destBranch = destCommitReader.read();
+        // #4 Read other branch commits
+        CommitReader              destCommitReader = getCommitFileReader(inputDirectory + File.separator + destBranchName);
+        Map<String, List<Commit>> destBranch       = destCommitReader.read();
 
-        //#5 Find missing commits
+        // #5 Find missing commits
+        processMissingCommits(srcBranch, destBranch, inputDirectory);
+
+        // #6 if requested generated debug files
+        if (enableDebugFiles) {
+            generateDebugFiles(srcBranch, destBranch, inputDirectory);
+        }
+
+        System.out.println("==================== End ==================== ");
+    }
+
+    private static void generateDebugFiles(final Map<String, List<Commit>> srcBranch, final Map<String, List<Commit>> destBranch, final String inputDirectory) {
+        Map<String, List<Commit>> targetCommits = new LinkedHashMap<>(destBranch);
+
+        StringBuilder output = new StringBuilder();
+        output.append("Commit Subject" + "\t");
+        output.append("Source Commit Hash" + "\t");
+        output.append("Target Commit Hash" + "\n");
+
+        for (String commitSubject : srcBranch.keySet()) {
+            for (Commit srcCommit : srcBranch.get(commitSubject)) {
+                StringBuilder line = new StringBuilder();
+                line.append(commitSubject + "\t");
+                line.append(srcCommit.getHash() + "\t");
+
+                List<Commit> trgCommitList = targetCommits.remove(commitSubject);
+
+                if (Objects.nonNull(trgCommitList)) {
+                    for (Commit trgCommit : trgCommitList) {
+                        StringBuilder tmp = new StringBuilder(line);
+                        tmp.append(trgCommit.getHash() + "\n");
+                        output.append(tmp);
+                    }
+                } else {
+                    output.append(line + "\n");
+                }
+            }
+        }
+
+        // remaining target commits
+        for (String commitSubject : targetCommits.keySet()) {
+            for (Commit commit: targetCommits.get(commitSubject)) {
+                StringBuilder line = new StringBuilder();
+                line.append(commitSubject + "\t" + "\t");
+                line.append(commit.getHash() + "\n");
+                output.append(line);
+            }
+        }
+
+        // dump into file
+        try (FileWriter fw = new FileWriter(inputDirectory + "commit-debug-report.csv")) {
+            fw.write(output.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void processMissingCommits(final Map<String, List<Commit>> srcBranch, final Map<String, List<Commit>> destBranch, final String inputDirectory) {
         Properties prop = new Properties();
         prop.put(Constants.IGNORE_USERS_PROPS_KEY, "Jenkins,Jenkins User");
         prop.put(Constants.IGNORE_KEYWARDS_PROPS_KEY, Constants.IGNORE_KEYWARDS_DEFAULT);
@@ -71,15 +135,12 @@ public class App {
             line.append(commit.getSubject() + "\n");
             output.append(line);
         }
-//        System.out.println(output);
 
         try (FileWriter fw = new FileWriter(inputDirectory + "probable-missing-commits.csv")) {
             fw.write(output.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println("==================== End ==================== ");
     }
 
     private static void validateArguments(String[] args) {
@@ -91,6 +152,8 @@ public class App {
     private static CommitReader getCommitFileReader(final String inputFilePath) {
         Properties props = new Properties();
         props.setProperty(Constants.INPUT_FILE_PATH_PROPS_KEY, inputFilePath);
+        props.setProperty(Constants.IGNORE_USERS_PROPS_KEY, "Jenkins,Jenkins User");
+        props.setProperty(Constants.IGNORE_KEYWARDS_PROPS_KEY, Constants.IGNORE_KEYWARDS_DEFAULT);
 
         CommitReader commitReader = new CommitFileReader();
         commitReader.init(props);
--- src/main/java/com/kishor/git/commit/read/CommitFileReader.java
+++ src/main/java/com/kishor/git/commit/read/CommitFileReader.java
@@ -5,11 +5,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.Set;
 
 import com.kishor.git.Constants;
 import com.kishor.git.model.Commit;
@@ -21,10 +24,16 @@ import com.kishor.git.model.Commit;
  */
 public class CommitFileReader implements CommitReader {
     private Optional<String> fileName = null;
+    private Set<String> ignoreKeywordSet = new HashSet<>();
+    private Set<String> ignoreUserSet    = new HashSet<>();
 
     @Override
     public void init(Properties props) {
+        Objects.requireNonNull(props, "Missing required parameter prop");
+
         fileName = Optional.ofNullable(props.getProperty(Constants.INPUT_FILE_PATH_PROPS_KEY));
+        addPropertiesToSet(props.getProperty(Constants.IGNORE_KEYWARDS_PROPS_KEY), ignoreKeywordSet);
+        addPropertiesToSet(props.getProperty(Constants.IGNORE_USERS_PROPS_KEY), ignoreUserSet);
     }
 
     @Override
@@ -34,20 +43,52 @@ public class CommitFileReader implements CommitReader {
         try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName.get())))) {
             reader.lines().forEach(l -> {
                 Commit comm = new Commit(l);
-                String key  = comm.getSubject();
 
-                List<Commit> cmmList = commits.get(key);
-                if (cmmList == null) {
-                    cmmList = new ArrayList<Commit>();
+                if (!ignoreCommit(comm)) {
+                    String key  = comm.getSubject();
+                    List<Commit> cmmList = commits.get(key);
+                    if (cmmList == null) {
+                        cmmList = new ArrayList<Commit>();
+                    }
+
+                    cmmList.add(comm);
+                    commits.put(key, cmmList);
                 }
-
-                cmmList.add(comm);
-                commits.put(key, cmmList);
             });
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return commits;
+    }
+
+    private boolean ignoreCommit(Commit commit) {
+        boolean ignoreCommitDueToSubject = false;
+        for (String ignoreSubjectKeyword: ignoreKeywordSet) {
+            if (commit.getSubject().startsWith(ignoreSubjectKeyword)) {
+                ignoreCommitDueToSubject = true;
+                break;
+            }
+        }
+
+        // check ignore user
+        boolean ignoreCommitDueToUser    = false;
+        if (ignoreUserSet.contains(commit.getAuthor())) {
+            ignoreCommitDueToUser = true;
+        }
+
+        return ignoreCommitDueToSubject || ignoreCommitDueToUser;
+    }
+
+    private void addPropertiesToSet(String valuesStr, Set<String> set) {
+        if (Objects.nonNull(valuesStr)) {
+            String[] values = valuesStr.split(",");
+
+            if (Objects.nonNull(values)) {
+                for (int index = 0; index < values.length; index++) {
+                    set.add(values[index]);
+                }
+            }
+        }
     }
 }
